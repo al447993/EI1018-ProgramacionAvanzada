8ee7e1705f4796e2cdb4ca0691a35dc2b0a73dd5
al447993 <al447993@uji.es>
2026-04-14 11:30:57 +0200

    Add files via upload

--- src/main/java/es/uji/al447993/clasificarFlores/tests/TestSuite.java
+++ src/main/java/es/uji/al447993/clasificarFlores/tests/TestSuite.java
@@ -1,7 +1,3 @@
-//COMENTARIO: no hemos conseguido que funcione, hemos arrancado
-//todos los tests a la vez a través de la carpeta que los contiene
-//(Click derecho en carpeta tests - RUN Tests in 'es.uji.al447993.clasificarFlores.tests')
-
 /*package es.uji.al447993.clasificarFlores.tests;
 
 import org.junit.platform.suite.api.IncludeClassNamePatterns;
@@ -13,9 +9,8 @@ import org.junit.platform.suite.api.SuiteDisplayName;
 
 @Suite
 @SuiteDisplayName("Lanzar todos los tests")
-@SelectPackages({"es.uji.al447993.clasificarFlores.KNN", "es.uji.al447993.clasificarFlores.CSV;", "es.uji.al447993.clasificarFlores.Table"})
+@SelectPackages("es.uji.al447993.clasificarFlores.tests")
 @IncludeClassNamePatterns(".*Test")
 public class TestSuite {
 }
-*/
 
